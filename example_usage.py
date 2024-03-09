3ec9ac8fb3e85eeb3159265b8f9d8e77de9112fb
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-03-10 00:46:53 +0100

    Update example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -1,7 +1,12 @@
-# From outside of this module
+# Paste example outside of this module
 from py_stock_price_scraper.yahoo_scraper import YahooScraper
 
 scraper = YahooScraper()
-symbol = 'MSFT'
+symbol = ''
+
+while symbol == '':
+    symbol = input('Ticker symbol (case-sensitive) > ')
+
+print('Fetching price')
 price = scraper.get_stock_price(symbol)
 print(price)
